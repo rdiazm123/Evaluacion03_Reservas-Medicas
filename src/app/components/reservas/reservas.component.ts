5efe5600f0d24abb4ddc97199ae8507d82a201fb
Romane-Nuñez <romanenunez97@gmail.com>
2026-07-14 16:47:13 -0400

    avance de pueba

--- src/app/components/reservas/reservas.component.ts
+++ src/app/components/reservas/reservas.component.ts
@@ -1,12 +1,74 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-reservas',
   standalone: true,
-  imports: [],
-  templateUrl: './reservas.component.html',
-  styleUrl: './reservas.component.css'
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
+  templateUrl: './reservas.html',
+  styleUrl: './reservas.css'
 })
-export class ReservasComponent {
+export class Reservas {
+
+
+  reserva = {
+
+    nombre:'',
+    especialidad:'',
+    fecha:''
+
+  };
+
+
+  reservas:any[]=[];
+
+
+  ngOnInit(){
+
+    this.cargar();
+
+  }
+
+
+  guardar(){
+
+    this.reservas.push({...this.reserva});
+
+
+    localStorage.setItem(
+      'reservas',
+      JSON.stringify(this.reservas)
+    );
+
+
+    this.reserva = {
+
+      nombre:'',
+      especialidad:'',
+      fecha:''
+
+    };
+
+  }
+
+
+  cargar(){
+
+    const datos = localStorage.getItem('reservas');
+
+
+    if(datos){
+
+      this.reservas = JSON.parse(datos);
+
+    }
+
+  }
+
 
 }
