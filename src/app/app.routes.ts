5f0803ba040d8d22afeb60649aeb124aac197060
rdiazm123 <ruth.diaz.marchant@gmail.com>
2026-07-14 17:30:15 -0400

    Estructurar y configurar componentes navbar, home, about y reservas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ReservasComponent } from './components/reservas/reservas.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'reservas', component: ReservasComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }
+];
--- src/app/components/reservas/reservas.component.ts
+++ src/app/components/reservas/reservas.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reservas',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './reservas.component.html',
   styleUrl: './reservas.component.css'
 })
-export class ReservasComponent {
+export class ReservasComponent implements OnInit {
+  reserva = {
+    fecha: ''
+  };
+  fechaActual: string = '';
 
+  ngOnInit(): void {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const dd = String(today.getDate()).padStart(2, '0');
+    this.fechaActual = `${yyyy}-${mm}-${dd}`;
+  }
 }
